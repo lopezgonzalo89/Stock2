0cf3ea2d7ea184281cf68ce1d7693c2b42e8f05a
lopezgonzalo89@ <lopezgonzalo.sm89@gmail.com>
2019-10-11 04:51:36 -0300

    base de datos con ip

--- src/java/Stock/Conecction/Conexion.java
+++ src/java/Stock/Conecction/Conexion.java
@@ -8,8 +8,8 @@ public class Conexion {
     public static Connection getConnection() throws ClassNotFoundException, SQLException {
         Connection dataBaseConnection;
             Class.forName("com.mysql.cj.jdbc.Driver");
-            String sourceURL = "jdbc:mysql://localhost:3306/choppin_db?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
-            dataBaseConnection = DriverManager.getConnection(sourceURL, "root", "");
+            String sourceURL = "jdbc:mysql://127.0.0.1:3306/choppin_db?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+            dataBaseConnection = DriverManager.getConnection(sourceURL, "Gonz", "sql2019");
             System.out.println("Conectado a la base de datos");        
         return dataBaseConnection;
     }
