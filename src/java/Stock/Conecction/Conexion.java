f32064924be860f4f4df1f820f6350d5dc8eae60
lopezgonzalo89@ <lopezgonzalo.sm89@gmail.com>
2019-10-29 04:04:46 -0300

    andando y ordenado

--- src/java/Stock/Conecction/Conexion.java
+++ src/java/Stock/Conecction/Conexion.java
@@ -24,6 +24,6 @@ public class Conexion {
     
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         Conexion con = new Conexion();
-        Conexion.getConnection();
+        Conexion.getConnectionSQLserver();
     }
 }
